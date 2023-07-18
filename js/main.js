863337229c8a63f1d3722c0bcaee61857b2f9e58
santhosh <killersanthosh0032@gmail.com>
2023-07-18 19:54:11 +0530

    multiplication

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Mulitiplication Table
+var a=prompt("enter a value");
+var n=prompt("Enter n Value");
+var b=1;
+var c;
+while(b<=n)
+	{
+	c=b*a;
+	document.write(b+"*"+a+"="+c+"<br>");
+	b++;
+	}
---
+++ js/media.js
@@ -0,0 +1,11 @@
+
+var n=prompt("Enter n Value");
+var a=5;
+var b=1;
+var c;
+while(b<=n)
+	{
+	c=b*a;
+	document.write(b+"*"+a+"="+c+"<br>");
+	b++;
+	}
